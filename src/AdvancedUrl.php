96325dbad3213a4fd2ad26b4745dbf6eb1725239
Sebastian Bossarte <kommentgames@gmail.com>
2017-12-05 15:54:12 -0600

    Created bare bones plugin.

---
+++ src/AdvancedUrl.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace charliedevelopment\advancedurl;
+
+use Craft;
+use craft\base\Plugin;
+
+class AdvancedUrl extends Plugin
+{
+	public function init()
+	{
+		parent::init();
+	}
+}
